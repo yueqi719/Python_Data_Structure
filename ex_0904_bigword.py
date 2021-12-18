8e2d21cd61a520a2d4f08903a8a0bfcc557241b3
yueqi719 <yueqi_w719@hotmail.com>
2021-12-18 20:11:18 +0800

    Done with this assignment

---
+++ ex_0904_bigword.py
@@ -0,0 +1,27 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+fh = open(name)
+counts = dict()
+emails = list()
+
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '):
+        continue
+    line = line.split()
+    emails.append(line[1])
+
+for email in emails:
+    counts[email] = counts.get(email,0) + 1
+
+bigcount = None
+bigemail = None
+for email, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigemail = email
+        bigcount = count
+
+print(bigemail, bigcount)
+#print(emails)
+        
