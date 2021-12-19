c4a586c146ca00b50a948c7e235e5aa9751eb88b
yueqi719 <yueqi_w719@hotmail.com>
2021-12-19 13:01:50 +0800

    list comprehension: make the code shorter but has the same result

--- tuples_reverse_sorted.py
+++ tuples_reverse_sorted.py
@@ -5,12 +5,13 @@ for line in fhand:
     for word in words:
         counts[word] = counts.get(word,0) + 1
 
-lst = list()
-for key,val in counts.items():
-    newtup = (val, key)
-    lst.append(newtup)
+#lst = list()
+#for key,val in counts.items():
+    #newtup = (val, key)
+    #lst.append(newtup)
 
-lst = sorted(lst, reverse = True)
+#lst = sorted(lst, reverse = True)
+lst = sorted([(v,k) for k,v in counts.items()],reverse = True)
 
 for val, key in lst[:10]:
     print(key,val)
